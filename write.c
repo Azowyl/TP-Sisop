89e98eadda4b90027b49b7301fd933e48c227eda
Azowyl <facugerstner_29@hotmail.com>
2018-06-10 10:58:52 -0300

    fix vga_write function

--- write.c
+++ write.c
@@ -1,14 +1,11 @@
 #include "decls.h"
 
-void vga_write(const char *s, int8_t linea, uint8_t color) { 
-volatile char *video = (volatile char*)0xB8000;
-    
-	int space_code = 0x20;
+#define SCREEN_WIDTH 80
 
-	for (int i = 0; i < linea * 80; i++) {
-		*video++ = space_code;
-		*video++ = 0;
-	}
+void vga_write(const char *s, int8_t linea, uint8_t color) { 
+	volatile char *video = (volatile char*)0xB8000;
+   
+	for (int i = 0; i < linea * SCREEN_WIDTH; i++) { video++; }
 
     while(*s != 0)
     {
