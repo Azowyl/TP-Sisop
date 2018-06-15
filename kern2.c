845030dfb6348d55a0330487bee7d5ed0fded450
Emiliano Prediger <ej.prediger@gmail.com>
2018-06-14 23:21:12 -0300

    vga_write() from stack1 works

--- kern2.c
+++ kern2.c
@@ -4,61 +4,59 @@
 #define USTACK_SIZE 4096
 
 void kmain(const multiboot_info_t *mbi) {
-    vga_write("kern2 loading.............", 8, 0x70);
+	vga_write("kern2 loading.............", 8, 0x70);
 
-    //two_stacks();
-    two_stacks_c();
+	//two_stacks();
+	two_stacks_c();
 }
 
 static uint8_t stack1[USTACK_SIZE] __attribute__((aligned(4096)));
 static uint8_t stack2[USTACK_SIZE] __attribute__((aligned(4096)));
 
 void two_stacks_c() {
-    // Inicializar al *tope* de cada pila.
-    uintptr_t *a = (uintptr_t *) &stack1[USTACK_SIZE - 1];
-    uintptr_t *b = (uintptr_t *) &stack2[USTACK_SIZE - 1];
+	// Inicializar al *tope* de cada pila.
+	uintptr_t *a = (uintptr_t *) &stack1[USTACK_SIZE - 1];
+	uintptr_t *b = (uintptr_t *) &stack2[USTACK_SIZE - 1];
 
-    // Preparar, en stack1, la llamada:
-    //vga_write("vga_write() from stack1", 15, 0x57);
+	// Preparar, en stack1, la llamada:
+	// vga_write("vga_write() from stack1", 15, 0x57);
 
-    *(a--) = 0x57;
-    *(a--) = 15;
-    *a = (uintptr_t) "vga_write() from stack1";
+	// AYUDA 1: se puede usar alguna forma de pre- o post-
+	// incremento/decremento, según corresponda:
+	//
+	//     *(a++) = ...
+	//     *(++a) = ...
+	//     *(a--) = ...
+	//     *(--a) = ...
 
-    task_exec((uintptr_t) vga_write, (uintptr_t) a);
+	// AYUDA 2: para apuntar a la cadena con el mensaje,
+	// es suficiente con el siguiente cast:
+	//
+	//   ... a ... = (uintptr_t) "vga_write() from stack1";
 
-    // AYUDA 1: se puede usar alguna forma de pre- o post-
-    // incremento/decremento, según corresponda:
-    //
-    //     *(a++) = ...
-    //     *(++a) = ...
-    //     *(a--) = ...
-    //     *(--a) = ...
+	*(a--) = 0x57;
+	*(a--) = 15;
+	*a = (uintptr_t) "vga_write() from stack1";
 
-    // AYUDA 2: para apuntar a la cadena con el mensaje,
-    // es suficiente con el siguiente cast:
-    //
-    //   ... a ... = (uintptr_t) "vga_write() from stack1";
+	task_exec((uintptr_t) vga_write, (uintptr_t) a);
 
-    // Preparar, en s2, la llamada:
-    /*vga_write("vga_write() from stack2", 16, 0xD0);
+	// Preparar, en s2, la llamada:
+	// vga_write("vga_write() from stack2", 16, 0xD0);
 
-    // AYUDA 3: para esta segunda llamada, usar esta forma de
-    // asignación alternativa:
-    b -= 3;
-    b[0] = ...
-    b[1] = ...
-    b[2] = ...
+	// AYUDA 3: para esta segunda llamada, usar esta forma de
+	// asignación alternativa:
+	b -= 3;
+	b[0] = 0xD0;
+	b[1] = 16;
+	b[2] = (uintptr_t) "vga_write() from stack2";
 
-    // Primera llamada usando task_exec().
-    task_exec((uintptr_t) vga_write, (uintptr_t) s1);
+	// Primera llamada usando task_exec().
+	task_exec((uintptr_t) vga_write, (uintptr_t) b);
 
-    // Segunda llamada con ASM directo. Importante: no
-    // olvidar restaurar el valor de %esp al terminar, y
-    // compilar con: -fasm -fno-omit-frame-pointer.
-    asm("...; call *%1; ..."
-        : // no outputs
-        : "r"(s2), "r"(vga_write));
-
-    */
+	// Segunda llamada con ASM directo. Importante: no
+	// olvidar restaurar el valor de %esp al terminar, y
+	// compilar con: -fasm -fno-omit-frame-pointer.
+	asm("...; call *%1; ..."
+		: // no outputs
+		: "r"(s2), "r"(vga_write));
 }
